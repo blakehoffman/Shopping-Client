7a67190a1cb597531cc8b75fffeabd680dd95c3b
blakehoffman <hoffman9134>
2022-02-18 09:45:00 -0600

    more work

--- shopping-client/src/app/services/api/api.service.ts
+++ shopping-client/src/app/services/api/api.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { AddCartProductDTO } from 'src/app/dtos/add-cart-product-dto';
+import { HttpResultDTO } from 'src/app/dtos/http-result-dto';
 import { environment } from 'src/environments/environment';
 import { AlertService } from '../alert/alert.service';
 import { HttpService } from '../http/http.service';
@@ -8,23 +11,63 @@ import { HttpService } from '../http/http.service';
     providedIn: 'root'
 })
 export class ApiService {
-
     private readonly _apiUrl = `${environment.baseUrl}`;
 
     constructor(
         private _alertService: AlertService,
         private _httpService: HttpService) { }
 
+    private handleError(error: any): Observable<any> {
+        this._alertService.error(error.message)
+        return throwError(error);
+    }
+
+    addProductToCart(cartID: string, addCartProductDTO: AddCartProductDTO): Observable<HttpResultDTO> {
+        return this._httpService.post(`${this._apiUrl}carts/${cartID}/products/add`, JSON.stringify(addCartProductDTO))
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
+    }
+
     createCart(id: string): Observable<any> {
-        return this._httpService.post(`${this._apiUrl}carts/create`, id);
+        return this._httpService.post(`${this._apiUrl}carts/create`, id)
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
     }
 
     getCart(): Observable<any> {
-        return this._httpService.get(`${this._apiUrl}carts`);
+        return this._httpService.get(`${this._apiUrl}carts`)
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
     }
 
     getProduct(productId: string): Observable<any> {
-        return this._httpService.getUnauthorized(`${this._apiUrl}products/${productId}`);
+        return this._httpService.getUnauthorized(`${this._apiUrl}products/${productId}`)
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
     }
 
     getProducts(categoryId?: string): Observable<any> {
@@ -34,6 +77,26 @@ export class ApiService {
             urlParams = `?categoryId=${categoryId}`;
         }
 
-        return this._httpService.getUnauthorized(`${this._apiUrl}products${urlParams}`);
+        return this._httpService.getUnauthorized(`${this._apiUrl}products${urlParams}`)
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
+    }
+
+    deleteCartProduct(cartID: string, productID: string) {
+        return this._httpService.post(`${this._apiUrl}carts/${cartID}/products/delete`, productID)
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
     }
 }
--- shopping-client/src/app/services/cart/cart.service.ts
+++ shopping-client/src/app/services/cart/cart.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { CartDTO } from 'src/app/dtos/cart-dto';
 
 import { CartProductDTO } from 'src/app/dtos/cart-product-dto';
-import { AlertService } from '../alert/alert.service';
 import { ApiService } from '../api/api.service';
 import { AuthService } from '../auth/auth.service';
 import { v4 } from 'uuid';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { AlertService } from '../alert/alert.service';
+import { AddCartProductDTO } from 'src/app/dtos/add-cart-product-dto';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CartService {
-    cart: CartDTO | undefined;
+    cartId: string | undefined;
+    products: Array<CartProductDTO> = [];
 
     constructor(
         private _alertService: AlertService,
@@ -20,48 +24,117 @@ export class CartService {
         this.loadCart();
     }
 
-    private createCart(): void {
-        let id = v4();
 
-        this._apiService.createCart(id)
-            .subscribe()
+    private handleError(error: any): Observable<any> {
+        this._alertService.error(error.message)
+        return throwError(error);
+    }
+
+    private createCart(): Observable<boolean> {
+        let newCartId = v4();
+
+        return this._apiService.createCart(newCartId)
+            .pipe(
+                tap(data => {
+                    if (data.succeeded) {
+                        this.cartId = newCartId;
+
+                        return true;
+                    }
+
+                    this._alertService.error(data.errors.join('\n'));
+                    return false;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
+    }
+
+    private getUserCart(): Observable<CartDTO> {
+        return this._apiService.getCart()
+            .pipe(
+                tap(data => {
+                    return data;
+                }),
+                catchError((error) => {
+                    return this.handleError(error);
+                })
+            );
     }
 
     private loadCart(): void {
         let cartString = localStorage.getItem("cart");
-        
+
         if (!cartString) {
             return;
         }
 
         let cart: CartDTO = JSON.parse(cartString);
-        this.cart = cart;
+        this.cartId = cart.id;
+        this.products = cart.products;
     }
 
-    addProductToCart(cartProductToAdd: CartProductDTO): void {
-        let cartProduct = this.cart.products.find(product => product.id == cartProductToAdd.id);
+    addProductToCart(cartProductToAdd: CartProductDTO): Observable<any> {
+        //if current user's cart isn't fetched, fetch it if they are logged in
+        if (this.cartId == undefined && this._authService.isLoggedIn) {
+            this.getUserCart().subscribe(cart => {
+                if (cart) {
+                    this.cartId = cart.id;
+                    this.products = cart.products;
+                }
+            });
+        }
+
+        let addCartProductDTO: AddCartProductDTO = {
+            id: cartProductToAdd.id,
+            quantity: cartProductToAdd.quantity
+        };
+
+        //if they are logged in, add product to cart on backend
+        if (this._authService.isLoggedIn) {
+            return this._apiService.addProductToCart((this.cartId as string), addCartProductDTO).pipe(
+                tap(result => {
+                    if (!result.succeeded) {
+                        this._alertService.error(result.errors.join('\n'));
+                    }
+                }),    
+                catchError((error) => { return this.handleError(error) })
+            );
+        }
+
+        let cartProduct = this.products.find(product => product.id == cartProductToAdd.id);
 
         //if product already exists in cart, don't add duplicate.  Just add new quantity
         if (cartProduct) {
             cartProduct.quantity += cartProductToAdd.quantity;
         }
         else {
-            this.cart.products.push(cartProductToAdd);
+            this.products.push(cartProductToAdd);
         }
+
+        return of();
     }
 
-    getUserCart(): void {
-        if (this._authService.isLoggedIn) {
-            this._apiService.getCart()
-                .subscribe(result => this.cart = result)
+    getUserCartAndMergeExistingCart(): void {
+        let userCart: CartDTO | undefined;
+
+        this.getUserCart().subscribe(cart => {
+            if (cart) {
+                userCart = cart;
+            }
+        });
+
+        if (!userCart) {
+            let userCartCreated = this.createCart();
         }
     }
 
     removeProductFromCart(cartProductToRemove: CartProductDTO): void {
-        let cartProduct = this.cart.products.find(product => product.id == cartProductToRemove.id);
+        let cartProduct = this.products.find(product => product.id == cartProductToRemove.id);
 
         if (cartProduct) {
-            this.cart.products.splice(this.cart.products.indexOf(cartProduct), 1);
+            this.products.splice(this.products.indexOf(cartProduct), 1);
         }
     }
 }
